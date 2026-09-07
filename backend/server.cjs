aa58cfcc6dc58d23cd325e4d4c377ffe0ad723dd
nareshvusm-gif <naresh.vusm@gmail.com>
2026-09-08 00:22:17 +0530

    adding server file chnage

--- backend/server.cjs
+++ backend/server.cjs
@@ -21,6 +21,9 @@ app.get("/students", (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log("Backend running at http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Backend running on port ${PORT}`);
 });
+
